b327de9b5b3c7f5d7915c1dd7bc27cd1036d6f55
Le Duy <ngocduy2607@gmail.com>
2024-08-26 09:39:54 +0700

    add sever.py file

--- sever.py
+++ sever.py
@@ -2,9 +2,11 @@ import http.server
 import socketserver
 
 PORT = 8000
+# Địa chỉ IP của máy chủ, có thể sử dụng '0.0.0.0' để cho phép tất cả các địa chỉ IP truy cập
+HOST = '0.0.0.0'
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
+    print(f"Serving at http://{HOST}:{PORT}")
     httpd.serve_forever()
